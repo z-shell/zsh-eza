22707953ad0116cd8619a7b2cc9e806d885f6c2f
Salvydas Lukosius <sall@w-ss.io>
2022-04-27 14:29:13 +0100

    workspace(:tada:)

---
+++ zsh-exa.plugin.zsh
@@ -0,0 +1,35 @@
+# -*- mode: zsh; sh-indentation: 2; indent-tabs-mode: nil; sh-basic-offset: 2; -*-
+# vim: ft=zsh sw=2 ts=2 et
+#
+# Copyright (c) 2022 Salvydas Lukosius
+#
+# Standardized $0 Handling
+# https://z.digitalclouds.dev/community/zsh_plugin_standard/#zero-handling
+
+0=${${ZERO:-${0:#$ZSH_ARGZERO}}:-${(%):-%N}}
+0=${${(M)0:#/*}:-$PWD/$0}
+
+if [[ ${zsh_loaded_plugins[-1]} != */zsh-exa && -z ${fpath[(r)${0:h}]} ]] {
+  fpath+=( "${0:h}" )
+}
+
+.load-aliases() {
+  alias ls='exa ${exa_params}'
+  alias l='exa --git-ignore ${exa_params}'
+  alias ll='exa --all --header --long ${exa_params}'
+  alias llm='exa --all --header --long --sort=modified ${exa_params}'
+  alias la='exa -lbhHigUmuSa'
+  alias lx='exa -lbhHigUmuSa@'
+  alias lt='exa --tree'
+  alias tree='exa --tree'
+}
+
+if ! (( $+commands[exa] )); then
+  print "exa not found on path. Please install exa before using this plugin." >&2
+  return 1
+else
+  emulate -L zsh
+  setopt extended_glob warn_create_global typeset_silent no_short_loops rc_quotes no_auto_pushd
+  local exa_params=('--git' '--icons' '--classify' '--group-directories-first' '--time-style=long-iso' '--group' '--color-scale')
+  .load-aliases
+fi
