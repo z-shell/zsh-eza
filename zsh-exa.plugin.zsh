a1267ededd5fa858149bfdbd7b374cb42e8d041d
Salvydas Lukosius <sall@w-ss.io>
2022-04-28 19:18:51 +0100

    Update zsh-exa.plugin.zsh

--- zsh-exa.plugin.zsh
+++ zsh-exa.plugin.zsh
@@ -13,7 +13,10 @@ if [[ ${zsh_loaded_plugins[-1]} != */zsh-exa && -z ${fpath[(r)${0:h}]} ]] {
   fpath+=( "${0:h}" )
 }
 
-typeset -g exa_params=('--git' '--icons' '--classify' '--group-directories-first' '--time-style=long-iso' '--group' '--color-scale')
+emulate -L zsh
+setopt extended_glob warn_create_global typeset_silent no_short_loops rc_quotes no_auto_pushd
+
+typeset -g exa_params=('--git' '--icons' '--classify' '--group-directories-first' '--time-style=long-iso' '--group' '--color-scale')  
 
 .alias-exa() {
   alias ls='exa ${exa_params}'
@@ -34,8 +37,7 @@ if ! (( $+commands[exa] )); then
   print "exa not found. Please install exa before using this plugin." >&2
   return 1
 else
-  emulate -L zsh
-  setopt extended_glob warn_create_global typeset_silent no_short_loops rc_quotes no_auto_pushd
   .alias-exa
+  declare -a chpwd_functions
+  [[ ${chpwd_functions[(r).auto-exa]} == .auto-exa ]] || chpwd_functions=( .auto-exa $chpwd_functions )
 fi
-[[ ${chpwd_functions[(r).auto-exa]} == .auto-exa ]] || chpwd_functions=( .auto-exa $chpwd_functions )
