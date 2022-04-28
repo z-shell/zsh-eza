9eb5aedb8a266658f90dd62962a498c71b2436b6
Salvydas Lukosius <sall@w-ss.io>
2022-04-28 18:35:45 +0100

    Update zsh-exa.plugin.zsh

--- zsh-exa.plugin.zsh
+++ zsh-exa.plugin.zsh
@@ -30,13 +30,12 @@ typeset -g exa_params=('--git' '--icons' '--classify' '--group-directories-first
   exa ${exa_params}
 }
 
-emulate -L zsh
-setopt extended_glob warn_create_global typeset_silent no_short_loops rc_quotes no_auto_pushd
-
 if ! (( $+commands[exa] )); then
   print "exa not found. Please install exa before using this plugin." >&2
   return 1
 else
+  emulate -L zsh
+  setopt extended_glob warn_create_global typeset_silent no_short_loops rc_quotes no_auto_pushd
   .alias-exa
-  [[ ${chpwd_functions[(r).auto-exa]} == .auto-exa ]] || chpwd_functions=( .auto-exa $chpwd_functions )
 fi
+[[ ${chpwd_functions[(r).auto-exa]} == .auto-exa ]] || chpwd_functions=( .auto-exa $chpwd_functions )
