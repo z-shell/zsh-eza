43743caa1688a461867505fca70cf0a491fe8e12
Salvydas Lukosius <sall@w-ss.io>
2022-05-12 22:22:09 +0100

    Update zsh-exa.plugin.zsh

--- zsh-exa.plugin.zsh
+++ zsh-exa.plugin.zsh
@@ -20,7 +20,6 @@ set_params() {
   else
     emulate -L zsh
     setopt extended_glob
-    # warn_create_global typeset_silent no_short_loops rc_quotes no_auto_pushd
     exa_params=('--git' '--icons' '--classify' '--group-directories-first' '--time-style=long-iso' '--group' '--color-scale')
 
     alias ls='exa ${exa_params}'
@@ -38,5 +37,11 @@ auto-exa() {
   command exa ${exa_params}
 }
 
+# Variable to enable cd list directories. Value 0 is the default.
+local enable_autocd=0
+[[ "$AUTOCD" = <-> ]] && enable_autocd="$AUTOCD"
+
 set_params
-[[ ${chpwd_functions[(r)auto-exa]} == auto-exa ]] || chpwd_functions=( auto-exa $chpwd_functions )
+if [[ "$AUTOCD" = "1" ]]; then
+  [[ ${chpwd_functions[(r)auto-exa]} == auto-exa ]] || chpwd_functions=( auto-exa $chpwd_functions )
+fi
