a04b4bd5494f731ad59e6a701dbb804c8b2a6a8b
Salvydas Lukosius <sall@w-ss.io>
2022-07-30 20:55:30 +0100

    modified:   .editorconfig 	modified:   .gitattributes 	modified:   .trunk/.gitignore 	modified:   .trunk/trunk.yaml 	new file:   "functions/\342\206\222zsh-exa" 	modified:   zsh-exa.plugin.zsh

--- zsh-exa.plugin.zsh
+++ zsh-exa.plugin.zsh
@@ -3,44 +3,13 @@
 #
 # Copyright (c) 2022 Salvydas Lukosius
 #
-# Standardized $0 Handling
-# https://z.digitalclouds.dev/community/zsh_plugin_standard/#zero-handling
-
+# Zsh Plugin Standard
+# https://wiki.zshell.dev/community/zsh_plugin_standard#zero-handling
 0="${ZERO:-${${0:#$ZSH_ARGZERO}:-${(%):-%N}}}"
 0="${${(M)0:#/*}:-$PWD/$0}"
 
-if [[ ${zsh_loaded_plugins[-1]} != */zsh-exa && -z ${fpath[(r)${0:h}]} ]] {
-  fpath+=( "${0:h}" )
+if [[ $PMSPEC != *f* ]] {
+  fpath+=( "${0:h}/functions" )
 }
 
-set_params() {
-  if ! (( $+commands[exa] )); then
-    print "exa not found. Please install exa before using this plugin." >&2
-    return 1
-  else
-    builtin emulate -L zsh -o extended_glob
-    exa_params=('--git' '--icons' '--classify' '--group-directories-first' '--time-style=long-iso' '--group' '--color-scale')
-
-    alias ls='exa ${exa_params}'
-    alias l='exa --git-ignore ${exa_params}'
-    alias ll='exa --all --header --long ${exa_params}'
-    alias llm='exa --all --header --long --sort=modified ${exa_params}'
-    alias la='exa -lbhHigUmuSa'
-    alias lx='exa -lbhHigUmuSa@'
-    alias lt='exa --tree'
-    alias tree='exa --tree'
-  fi
-}
-
-auto-exa() {
-  command exa ${exa_params}
-}
-
-# Variable to enable cd list directories. Value 0 is the default.
-local enable_autocd=0
-[[ "$AUTOCD" = <-> ]] && enable_autocd="$AUTOCD"
-
-set_params
-if [[ "$enable_autocd" = "1" ]]; then
-  [[ ${chpwd_functions[(r)auto-exa]} == auto-exa ]] || chpwd_functions=( auto-exa $chpwd_functions )
-fi
+autoload -Uz →zsh-exa; →zsh-exa
