37358693996841d8729b63585ad4670076f2cf4b
Salvydas Lukosius <sall@w-ss.io>
2022-08-13 06:14:41 +0100

    perf: 🚀 Minor refactor + trunk upgrade

--- zsh-exa.plugin.zsh
+++ zsh-exa.plugin.zsh
@@ -8,8 +8,24 @@
 0="${ZERO:-${${0:#$ZSH_ARGZERO}:-${(%):-%N}}}"
 0="${${(M)0:#/*}:-$PWD/$0}"
 
-if [[ $PMSPEC != *f* ]] {
+# https://wiki.zshell.dev/community/zsh_plugin_standard#standard-plugins-hash
+typeset -gA Plugins
+Plugins[ZSH_EXA]="${0:h}"
+Plugins[ZSH_EXA_VERSION]="1.0.0"
+
+# https://wiki.zshell.dev/community/zsh_plugin_standard#funtions-directory
+if [[ $PMSPEC != *f* ]]; then
   fpath+=( "${0:h}/functions" )
+fi
+
+autoload -Uz .zsh-exa
+
+# Load plugin
+(( ${+functions[.zsh-exa]} )) && {
+  .zsh-exa; exit_code=$?
 }
 
-autoload -Uz →zsh-exa; →zsh-exa
+(( exit_code )) && {
+  print "Error loading zsh-exa plugin, exit code: $exit_code"
+  exit $exit_code
+}
